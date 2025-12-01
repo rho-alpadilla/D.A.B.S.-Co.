4627d3ebf281981a6209cdc8d97dff98fd462b27
rho-alpadilla <rhoalpadilla10@gmail.com>
2025-12-01 22:53:41 +0800

    test

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -182,7 +182,7 @@ const AdminPanel = () => {
       <div className="min-h-screen bg-gray-50 flex items-center justify-center">
         <div className="text-center">
           <h1 className="text-4xl font-bold text-red-600">Access Denied</h1>
-          <p className="text-gray-600 mt-2">You must be an admin to view this page.</p>
+          <p className="text-gray-600 mt-2">You must be an admin to view this page. </p>
         </div>
       </div>
     );
